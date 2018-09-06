4063692558e3f97d15c0fa6c667bde6d02d7e54f
zhaoxiaojie123 <zhaoxiaojie@gvitech.com>
2018-09-06 14:23:51 +0800

    优化配置文件，改变编码方式

--- src/main/java/org/gt/config/DruidConfiguration.java
+++ src/main/java/org/gt/config/DruidConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.PropertySource;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource(value = "classpath:druid.properties")
+@PropertySource(value = "classpath:config/druid.properties")
 public class DruidConfiguration {
 
     @Bean(destroyMethod = "close", initMethod = "init")
